afc35441c80a03088e1dd3b0bf35e0dbd46a7886
KoraReinolds <s.shurin94@gmail.com>
2023-06-14 19:06:24 +0300

    add FadedImg component - make smooth gradient from top to bottom for image in slot

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -3,10 +3,10 @@
 
 <template>
   <div class="h-screen sm:h-[600px] relative flex justify-center">
-    <img src="../assets/img/massage_main.png" alt="Массаж в салоне Николе" title="Массаж"
-      class="h-full w-full object-cover absolute inset-0">
-    <div class="w-full h-full bg-black absolute inset-0 opacity-50" />
-    <div class="w-full h-full bg-gradient-to-t from-black via-transparent via-30% absolute inset-0" />
+    <FadedImg>
+      <img src="../assets/img/massage_main.png" alt="Массаж в салоне Николе" title="Массаж"
+        class="h-full w-full object-cover absolute inset-0">
+    </FadedImg>
     <div class="relative px-8 flex flex-col items-center justify-center h-full">
       <div class="text-3xl font-bold italic text-center text-main">
         Скидка <span class="text-5xl text-secondary">20%</span>
