aaaf5064b61c2ee1a3323447285c09139ce82447
KoraReinolds <s.shurin94@gmail.com>
2023-06-25 19:59:02 +0300

    change Btn component layout - remove slot - add icon property

--- components/Btn.vue
+++ components/Btn.vue
@@ -2,24 +2,34 @@
 const props = defineProps({
   link: String,
   title: String,
+  icon: String,
   type: {
     type: String,
     required: false,
   }
 })
-let btnClasses = 'text-xl'
-if (props.type === 'lg') {
-  btnClasses = 'text-2xl'
+let btnClasses = {
+  fontSize: 'text-base',
+  height: 'h-12',
 }
+if (props.type === 'lg') {
+  btnClasses.fontSize = 'text-xl'
+  btnClasses.height = 'h-16'
+}
+let iconClasses = 'w-6 h-6 ml-3'
+if (props.type === 'lg') {
+  iconClasses = 'w-8 h-8 ml-4'
+}
+console.log(props.type, btnClasses, props.icon)
 </script>
 
 <template>
   <div
-    :class="[btnClasses, 'cursor-pointer px-6 py-4 text-dark rounded-2xl font-bold italic text-center bg-main flex items-center justify-center']">
-    <a :href="props.link">
+    :class="[btnClasses.height, 'max-w-max cursor-pointer px-6 text-dark rounded-2xl font-bold italic text-center bg-main flex items-center justify-center text-2xl']">
+    <a :class="btnClasses.fontSize" :href="props.link">
       {{ props.title }}
     </a>
-    <slot></slot>
+    <img v-if="props.icon" :class="iconClasses" :src="props.icon">
   </div>
 </template>
 
