dd950dbc3ebb82432b3293e1b1cb31a3c2a54091
KoraReinolds <s.shurin94@gmail.com>
2023-07-02 13:50:15 +0300

    add images to review section

--- components/Reviews.vue
+++ components/Reviews.vue
@@ -2,30 +2,40 @@
 import { tryOnMounted } from '@vueuse/core';
 const socialsImgs = import.meta.globEager('@/assets/img/socials/*.png');
 defineEmits(['scroll'])
+const reviewImgs = import.meta.globEager('@/assets/img/reviews/*.jpg');
 
 const reviews = [
   {
     ref: "olga",
+    // img: reviewImgs['/assets/img/reviews/review-olga.jpg'].default,
     master: "Ольга",
     area: "Депиляция",
     text: "Спасибо за чудесно проведенное время и результат!!!\n Безболезненно, будто на массаж ходила!)))"
   }, {
     ref: "ksenia",
+    // img: reviewImgs['/assets/img/reviews/review-ksenia.jpg'].default,
     master: "Ксения",
     area: "Макияж",
     text: "Моему счастью нет предела! Я наконец то нашла мастера, который из работ не очень толковых мастеров сделала шикарные, а главное естественные брови! Ксюша - большая умничка! Брови - это твое)"
   }, {
     ref: "karina",
+    // img: reviewImgs['/assets/img/reviews/review-karina.jpg'].default,
     master: "Карина",
     area: "Массаж",
     text: "Добрый день, Карина прекрасный специалист своего дела! Во время сеанса забываешь обо всем и отключаешься от реальности, а сильные, нежные и чувственные руки помогают расслабить все мышцы тела и снять всё напряжение"
+  }, {
+    ref: "natalia",
+    // img: reviewImgs['/assets/img/reviews/review-natalia.jpg'].default,
+    master: "Наталия",
+    area: "Макияж",
+    text: "На работе оценили вашу работу и муж тоже) девчонки мои тоже сказали придут к вам после отпуска"
   },
 ]
 const reviewIndex = ref(0)
 let intervalId
 const resetInterval = () => {
   clearInterval(intervalId)
-  intervalId = setInterval(increase, 5000)
+  intervalId = setInterval(increase, 8000)
 }
 const increase = () => {
   resetInterval()
@@ -63,13 +73,14 @@ tryOnMounted(() => {
       -
       <span class="font-bold underline cursor-pointer">{{ r.area }}</span>
     </div>
-    <div class="flex flex-col gap-8 items-center w-full max-w-prose">
+    <div class="flex flex-col gap-8 items-center w-full max-w-md">
 
       <div class="flex flex-row justify-center items-center w-full gap-4">
         <Arrow class="h-full cursor-pointer rotate-180" @click="decrease" />
         <div ref="textRef" class="w-full flex items-center overflow-hidden">
           <div :style="textStyle" class="relative w-full shrink-0 text-center text-base italic" v-for="review in reviews">
-            {{ review.text }}
+            {{ r.text }}
+            <!-- <img class="object-contain h-full mx-auto" :src="r.img" alt="Ваши отзывы"> -->
           </div>
         </div>
         <Arrow class="h-full cursor-pointer" @click="increase" />
